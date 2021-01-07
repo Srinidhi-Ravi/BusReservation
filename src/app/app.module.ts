2f6d94127ae3c2fd5877fd4347260bc9184e9683
SivaK18 <sivakailash18@gmail.com>
2021-01-07 20:54:09 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,7 +42,7 @@ const routing = RouterModule.forRoot(routes);
     AdminComponent,
     JourneyhireComponent,
     SearchbusComponent,
-    ImgHoverComponent
+    ImgHoverComponent,
     BookingComponent
   ],
   imports: [
